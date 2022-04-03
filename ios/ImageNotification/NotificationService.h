9b95a947408b07575e08b77d56ae59976c74b016
Abdulrahaman <olatunde.abdulrahaman@gmail.com>
2022-04-03 12:42:33 +0100

    Apple integration of FCM

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Olatunde Abdulrahaman on 03/04/2022.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
